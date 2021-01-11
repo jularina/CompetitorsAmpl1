fa30f183b9a46fcd00137a7a2fce4c5424276fa7
Arina2707 <arina.odnoblyudova@mail.ru>
2021-01-11 11:51:52 +0300

    commit

--- scraper.py
+++ scraper.py
@@ -1,24 +1,38 @@
-import requests
 from selenium import webdriver
-from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+import time
 
-#url = input('Enter url: ')
+# --| Setup
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--window-size=1980,1020")
+browser = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe', options=options)
+
 url = 'https://spie.org/ExhibitorDatabase/Search?SSO=1'
-# response = requests.get(url)
-# soup = BeautifulSoup(response.text, 'html.parser')
-#
-# for link in soup.find_all('a'):
-#     print('*************')
-#     print(link)
-#     print(link.get('href'))
+browser.get(url)
+time.sleep(2)
 
-driver = webdriver.Chrome('C:/Users/Arina27/Downloads/chromedriver.exe')
-driver.get(url)
-page = driver.page_source
-# page = driver.execute_script('return document.body.innerHTML')
-soup = BeautifulSoup(page, 'html.parser')
+tags = browser.find_elements_by_class_name('formLink01')
+tags = tags[5::4][:10]
 
-for link in soup.find_all('a'):
-    print('*************')
-    print(link)
-    print(link.get('href'))
+for tag in tags:
+    url2 = browser.find_element_by_link_text(tag.text).get_attribute('href')
+    browser2 = webdriver.Chrome(executable_path=r'C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe',
+                                options=options)
+    browser2.get(url2)
+    try:
+        description = browser2.find_element_by_id('Main_lblCompanyDescription').text
+        link = browser2.find_elements_by_class_name('formText07')
+
+    except NoSuchElementException:
+        description = 'No description'
+        link = 'No link'
+
+    print('Company name: ', tag.text, '\n', 'Company description: ', description, '\n')
+
+    if isinstance(link, str):
+        print(link)
+    else:
+        print('Address: ', link[2].text.splitlines()[1:], '\n')
+        print('Website: ', link[3].text[9:])
